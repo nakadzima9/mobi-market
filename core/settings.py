6e34c6247e5c00176ab3f29ab66138955430f589
Nakadzima9 <pascal.german@yandex.ru>
2023-07-22 20:30:06 +0600

    something update

--- core/settings.py
+++ core/settings.py
@@ -19,8 +19,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
