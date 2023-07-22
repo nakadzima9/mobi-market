8e01c8523ed56722295b20b8d0a5a5a784509a3b
Nakadzima9 <pascal.german@yandex.ru>
2023-07-22 21:50:14 +0600

    some update settings

--- core/settings.py
+++ core/settings.py
@@ -199,7 +199,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 # Default primary key field type
